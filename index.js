7fe9bf987ca74375cb63794318bfccbdf3cd1914
hector <hdavila@thoughtworks.com>
2017-04-21 09:28:19 -0500

    mixing express with actions-on-google package

--- index.js
+++ index.js
@@ -1,45 +1,29 @@
-// var express = require('express');
-// var app = express();
-
-// app.set('port', (process.env.PORT || 5000));
-
-// app.use(express.static(__dirname + '/public'));
-
-// // views is directory for all template files
-// app.set('views', __dirname + '/views');
-// app.set('view engine', 'ejs');
-
-// app.get('/', function (request, response) {
-//   response.render('pages/index');
-// });
-
-// app.post('/', function (request, response) {
-
-// })
-
-// app.listen(app.get('port'), function () {
-//   console.log('Node app is running on port', app.get('port'));
-// });
-'use strict';
-
-process.env.DEBUG = 'actions-on-google:*';
+var express = require('express');
+var app = express();
 const Assistant = require('actions-on-google').ApiAiAssistant;
+app.set('port', (process.env.PORT || 5000));
 
-// [START YourAction]
-exports.yourAction = (req, res) => {
-  const assistant = new Assistant({request: req, response: res});
-  console.log('Request headers: ' + JSON.stringify(req.headers));
-  console.log('Request body: ' + JSON.stringify(req.body));
+app.use(express.static(__dirname + '/public'));
 
-  // Fulfill action business logic
-  function responseHandler (assistant) {
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function (request, response) {
+  response.render('pages/index');
+});
+
+app.post('/', function (req, res) {
+  const assistant = new Assistant({ request: req, response: res });
+  function responseHandler(assistant) {
     // Complete your fulfillment logic and send a response
     assistant.tell('Hello, World!');
   }
-
   const actionMap = new Map();
   actionMap.set('<API.AI_test>', responseHandler);
-
   assistant.handleRequest(actionMap);
-};
+})
 
+app.listen(app.get('port'), function () {
+  console.log('Node app is running on port', app.get('port'));
+});
