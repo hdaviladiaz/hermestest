0e4b0ad4ca3b0146123d30024478421d1fed4e2c
hector <hdavila@thoughtworks.com>
2017-04-24 09:46:26 -0500

    test1

---
+++ actions/user-information_action.js
@@ -0,0 +1,9 @@
+var user_servive = require('../custom_modules/user.js');
+var execute = function (request) {
+    console.log("-----------------------------------------------");
+    console.log(request);
+    console.log("-----------------------------------------------");
+    
+}
+
+exports.execute = execute;
