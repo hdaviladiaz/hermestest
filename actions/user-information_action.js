ec0a671cdc889a7faf8a856165363693c4920fd9
hector <hdavila@thoughtworks.com>
2017-04-24 09:52:05 -0500

    test2

--- actions/user-information_action.js
+++ actions/user-information_action.js
@@ -1,8 +1,8 @@
 var user_servive = require('../custom_modules/user.js');
 var execute = function (request) {
-    console.log("-----------------------------------------------");
-    console.log(request);
-    console.log("-----------------------------------------------");
+   return{
+       text: JSON.stringify(request)
+   }
     
 }
 
