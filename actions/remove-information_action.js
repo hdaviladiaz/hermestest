f6335720a3a82c461567e7d6a8d2d86cbdae1592
hector <hdavila@thoughtworks.com>
2017-04-25 11:17:40 -0500

    test31

--- actions/remove-information_action.js
+++ actions/remove-information_action.js
@@ -4,12 +4,17 @@ var execute = function (request) {
     var informationFields = request.result.parameters.informationFields;
     var user = userService.getCurrentUser();
     if (user) {
-        for (var i in informationFields) {
-            var field = informationFields[i];
-            user[field] = null;
+        if (informationFields) {
+            for (var i in informationFields) {
+                var field = informationFields[i];
+                user[field] = null;
+            }
+        }else{
+            userService.updateCurrentUser({});
         }
+
     }
-    var eventRequest = userInformation.createEventRequest("speech","OK");
+    var eventRequest = userInformation.createEventRequest("speech", "OK, information was updated");
     return eventRequest;
 }
 
---
+++ actions/speech_action.js
@@ -0,0 +1,10 @@
+var user_service = require('../custom_modules/user.js');
+var action_manager = require('../actions/action_manager.js');
+var execute = function (request) {
+    return {
+        text: request.customParameter || "Ok"
+    }
+}
+
+
+exports.execute = execute;
