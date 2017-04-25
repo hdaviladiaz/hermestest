b2d10c5f91a00a005512a596c7a4ce4f593b06ac
hector <hdavila@thoughtworks.com>
2017-04-25 11:22:18 -0500

    test33

--- actions/speech_action.js
+++ actions/speech_action.js
@@ -2,7 +2,7 @@ var user_service = require('../custom_modules/user.js');
 var action_manager = require('../actions/action_manager.js');
 var execute = function (request) {
     return {
-        text: request.parameters || request.customParameter || "Ok"
+        text: request.customParameters || request.result.parameters  || "Ok"
     }
 }
 
