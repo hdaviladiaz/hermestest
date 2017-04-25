145126772bc66b4c4b33e373d169a007de65860b
Schubert Rodriguez <scrodrig@thoughtworks.com>
2017-04-25 16:44:16 -0500

    Add text from JSON func

--- services/searchFlightService.js
+++ services/searchFlightService.js
@@ -1,10 +1,13 @@
-
 var Flight = require('../model/flight.js');
 var moment = require('moment');
 var patternFormat = 'YYYY-MM-DD';
 
 var searchFlights = function(origin, destination, date){
     return new Flight(origin, destination, date);
+}
+
+var searchFlightsToText = function (origin, destination, date){
+    var flight = searchFlights(origin, destination, date);
 }
 
 exports.searchFlights = searchFlights
