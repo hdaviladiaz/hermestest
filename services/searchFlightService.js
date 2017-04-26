153eaf5282333f38676580665422e652505c2429
hector <hdavila@thoughtworks.com>
2017-04-26 16:53:58 -0500

    test46

--- services/searchFlightService.js
+++ services/searchFlightService.js
@@ -4,21 +4,18 @@ var moment = require('moment');
 var searchFlightGestor = require('../gestor/searchFlightGestor.js');
 var storedPage = 1;
 var storedOrigin, storedDestination, storedDate;
-var searchFlights = function (origin, destination, date, page) {
-  page = page || storedPage;
-  origin = origin || storedOrigin;
-  destination = destination || storedDestination;
-  date = date || storedDate;
+var searchFlights = function (origin, destination, date) {
+  page = 1;
+  var momentDate = moment(date, "YYYY-MM-DD");
   var flight = searchFlightGestor.searchFlights(origin, destination, date, page, 2);
   var text = "There are no more flights.";
   if (!flight || !flight.trips || flight.trips.length == 0) {
     return text
   }
-  text = " I found in LATAM " + flight.pagination.count + " flights for "+humanize.naturalDay(date.unix())+" from " + origin + " to " + destination + ". ";
+  text = " I found in LATAM " + flight.pagination.count + " flights for "
+    + humanize.naturalDay(momentDate.unix()) + " from " + origin + " to " + destination + ". ";
   _.each(flight.trips, function (trip, index) {
-    var date = moment(trip.departure.date, "YYYY-MM-DD");
     text += humanize.ordinal(index + 1) + " for " + trip.price + " " + flight.currency + ".";
-    // + humanize.naturalDay(date.unix()) + ". "
   })
   storedPage = page;
   storedDate = date;
@@ -27,18 +24,16 @@ var searchFlights = function (origin, destination, date, page) {
   return text;
 }
 var moreFlights = function () {
-  storedPage =storedPage+1;
+  storedPage = storedPage + 1;
   var flight = searchFlightGestor.searchFlights(storedOrigin, storedDestination, storedDate, storedPage);
   var text = "There are no more flights.";
   if (!flight || !flight.trips || flight.trips.length == 0) {
-    storedPage =storedPage-1;
+    storedPage = storedPage - 1;
     return text
   }
   text = "";
   _.each(flight.trips, function (trip, index) {
-    var date = moment(trip.departure.date, "YYYY-MM-DD");
-    text += humanize.ordinal(index + 1 + (storedPage-1) * 2) + " for " + trip.price + " " + flight.currency + ".";
-    // + humanize.naturalDay(date.unix()) + ". "
+    text += humanize.ordinal(index + 1 + (storedPage - 1) * 2) + " for " + trip.price + " " + flight.currency + ".";
   })
   return text;
 }
