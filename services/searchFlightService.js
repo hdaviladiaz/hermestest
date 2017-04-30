3ddbe15c202ad12a5b838963e19cde0ceffe6c0c
hector <hdavila@thoughtworks.com>
2017-04-30 12:42:32 -0500

    test63

--- services/searchFlightService.js
+++ services/searchFlightService.js
@@ -78,8 +78,8 @@ var getFlightTime = function (index) {
   }
   else {
     if (flight.trips.length == 1) {
-      text += "The flight leaves at " + trip.departure.time
-        + " and arrives at " + trip.arrival.time + ". ";
+      text += "The flight leaves at " + flight.trips[0].departure.time
+        + " and arrives at " + flight.trips[0].arrival.time + ". ";
     } else {
       _.each(flight.trips, function (trip, index) {
         text += "The " + humanize.ordinal(index + 1) + " option leaves at " + trip.departure.time
